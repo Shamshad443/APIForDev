f87f9c8e9c06413146f0da590b5ea1a8abbfd96a
Shamshad443 <53820821+Shamshad443@users.noreply.github.com>
2019-08-06 17:42:38 +0530

    Update ValuesController.cs

--- DemoCICDWebApp/Controllers/ValuesController.cs
+++ DemoCICDWebApp/Controllers/ValuesController.cs
@@ -12,7 +12,7 @@ namespace DemoCICDWebApp.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "Hi iliyas" };
         }
 
         // GET api/values/5
